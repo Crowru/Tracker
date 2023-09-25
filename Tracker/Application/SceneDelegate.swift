458ea62ce5fab76cb1727f3777a35af81da68f9e
Ruslan <ruslanusmanov7779@gmail.com>
2023-09-25 22:09:26 +0300

    Created onboarding in the app

--- Tracker/Application/SceneDelegate.swift
+++ Tracker/Application/SceneDelegate.swift
@@ -14,8 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-
-        window?.rootViewController = TabBarController()
+        window?.rootViewController = OnboardingViewController(transitionStyle: .scroll)
         window?.makeKeyAndVisible()
     }
 }
